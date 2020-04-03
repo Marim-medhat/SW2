3f325e6f3e41ffe19460e6f764dc79b26481e744
mernaezzat <36450631+mernaezzat@users.noreply.github.com>
2020-04-03 17:17:46 +0200

    registerController

--- src/main/java/com/sw2/Repositories/Controller/registcontroller.java
+++ src/main/java/com/sw2/Repositories/Controller/registcontroller.java
@@ -1,14 +1,29 @@
 package com.sw2.Repositories.Controller;
 
+
+import java.util.List;
+
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import swe2onlinestore.services.registeration;
 @Controller
 public class registcontroller {
+	 @Autowired
+	    private registeration service;
+	 
 	
-@RequestMapping("/registuser")
-
-public String  registuser() {
-	return"registUser";
-}
-
-}
+	     
+	     @RequestMapping(value = "/regist", method = RequestMethod.POST)
+	     
+	     public String registuser(@ModelAttribute("user") User user) {
+	         service.registUser(user);
+	          
+	         return "redirect:/";
+	     }
+	 }
