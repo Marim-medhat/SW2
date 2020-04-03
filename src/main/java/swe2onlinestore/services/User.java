3312235eeeb76096399461c22c96f2c2be5eca91
Bssant <bassantnasser31@gmail.com>
2020-04-03 16:22:49 +0200

    edit class User

--- src/main/java/swe2onlinestore/services/User.java
+++ src/main/java/swe2onlinestore/services/User.java
@@ -1,53 +1,73 @@
 package swe2onlinestore.services;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+	import javax.persistence.Entity;
+	import javax.persistence.GeneratedValue;
+	import javax.persistence.GenerationType;
+	import javax.persistence.Id;
 
-@Entity
+	@Entity
 
-public class User {
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer id;
-	String type="";
-	String userName="";
-	String mail="";
-	int password=0;
-	
-	public User(String userName, String mail, int password) {
-		this.userName = userName;
-		this.mail = mail;
-		this.password = password;
+	public class User {
+		String type="";
+		String userName="";
+		String mail="";
+		String password="";
+		
+		
+		public User() {
+		}
+
+
+		public User(String userName, String mail, String password) {
+			this.userName = userName;
+			this.mail = mail;
+			this.password = password;
+		}
+		
+		@Id
+		@GeneratedValue(strategy=GenerationType.AUTO)
+		public String getId() {
+	        return mail;
+	    }
+
+
+		public String getType() {
+			return type;
+		}
+
+
+		public void setType(String type) {
+			this.type = type;
+		}
+
+
+		public String getUserName() {
+			return userName;
+		}
+
+
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
+
+
+		public String getMail() {
+			return mail;
+		}
+
+
+		public void setMail(String mail) {
+			this.mail = mail;
+		}
+
+
+		public String getPassword() {
+			return password;
+		}
+
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		
+
 	}
-
-
-
-	//getter
-			public String getName() {
-				return userName;
-			}
-			
-			public int getPassword() {
-				return password;
-			}
-			public String getmail() {
-				return mail;
-			}
-			 
-	    //setter
-			public void setName() {
-				this.userName=userName;
-				
-			}
-			public void setPassword() {
-				this.password=password;
-				
-			}
-			public void setMail() {
-				this.mail=mail;
-				
-			}
-	
-
-}
